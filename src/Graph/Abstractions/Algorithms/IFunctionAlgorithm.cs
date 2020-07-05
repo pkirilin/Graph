7bc7fd937026034f388999d07a9dc3c7e971e009
Pavel Kirilin <kirilin.pav@gmail.com>
2020-07-05 11:47:25 +0300

    Added algorithms abstractions

---
+++ src/Graph/Abstractions/Algorithms/IFunctionAlgorithm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Graph.Abstractions.Algorithms
+{
+    /// <summary>
+    /// Provides execution method for graph algorithm which returns some result
+    /// </summary>
+    /// <typeparam name="TGraph">Graph type</typeparam>
+    /// <typeparam name="TVertex">Graph vertex type</typeparam>
+    /// <typeparam name="TResult">Algorithm result type</typeparam>
+    interface IFunctionAlgorithm<TGraph, TVertex, TResult>
+        where TGraph : GraphBase<TVertex>
+        where TVertex : IComparable<TVertex>
+    {
+        /// <summary>
+        /// Executes algorithm on graph
+        /// </summary>
+        /// <param name="graph">Target graph</param>
+        /// <returns>Algorithm result</returns>
+        TResult Execute(TGraph graph);
+    }
+
+    /// <summary>
+    /// Provides execution method for graph algorithm which takes parameter and returns some result
+    /// </summary>
+    /// <typeparam name="TGraph">Graph type</typeparam>
+    /// <typeparam name="TVertex">Graph vertex type</typeparam>
+    /// <typeparam name="TResult">Algorithm result type</typeparam>
+    /// <typeparam name="TParameter">Input parameter for algorithm</typeparam>
+    interface IFunctionAlgorithm<TGraph, TVertex, TResult, TParameter>
+        where TGraph : GraphBase<TVertex>
+        where TVertex : IComparable<TVertex>
+    {
+        /// <summary>
+        /// Executes algorithm on graph
+        /// </summary>
+        /// <param name="graph">Target graph</param>
+        /// <param name="parameter">Input parameter for algorithm</param>
+        /// <returns>Algorithm result</returns>
+        TResult Execute(TGraph graph, TParameter parameter);
+    }
+}
